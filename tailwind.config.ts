b8cbbf414043c2af7cecefba4dfdedfeb2c2df57
Irina Dimcheva <dimchevairina@gmail.com>
2024-05-28 18:08:07 +0300

    Set up fonts and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'pale-lime-green': '#B4FF5C',
+        'anti-flash-white': '#F1F4F5',
+        'light-100': '#F4F4F4',
+        'light-200': '#EBEBEB',
+        'light-300': '#DCDCDC',
+        'dark-default': '#ADADAD',
+        'dark-500': '#7B7B7B',
+        'dark-600': '#464646',
+        'dark-700': '#353535',
+        'dark-800': '#0C0C0C',
+      },
+      screens: {
+        wide: '1440px',
       },
     },
   },
