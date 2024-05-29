72e2ef43a9e49ec031727892191b8a2a6b10eac9
Irina Dimcheva <dimchevairina@gmail.com>
2024-05-29 16:59:09 +0300

    Header mobile completed

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,61 +6,59 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className="bg-gradient-to-b from-primary from-0%  via-primary/30 via-40% to-transparent to-90%">
-      <div className="px-8">
-        <div className="max-w-screen-fit mx-auto">
-          <Navbar />
-          <section className="py-24">
-            <div className="w-[672px] mx-auto text-center">
-              <div className="relative">
-                <h1
-                  className={`${sfPro.className} text-[88px] font-bold leading-[110%]`}
-                >
-                  We love to find solutions
-                </h1>
-                <span className="absolute bottom-5 right-[37%]">
-                  <Image
-                    src="./images/Group.svg"
-                    alt=""
-                    width={267}
-                    height={37}
-                  />
-                </span>
-              </div>
-              <p className="text-natural-3 font-bold py-8">
-                Elevate your online presence with Spine cutting-edge solutions.
-                Discover limitless possibilities for your digital agency&apos;s
-                success.
-              </p>
-              <Button label="Start for free" />
+      <div className="max-w-screen-fit mx-auto px-6 xl:px-0">
+        <Navbar />
+        <section className="mt-8 -mb-24 md:my-24">
+          <div className="w-[672px] max-w-[90vw] mx-auto text-center px-4">
+            <div className="relative">
+              <h1
+                className={`${sfPro.className} text-[48px] md:text-[88px] font-bold leading-[110%]`}
+              >
+                We love to find solutions
+              </h1>
+              <span className="absolute bottom-3 md:bottom-5 w-[149px] right-[10%] md:w-[240px] md:right-[41%]">
+                <Image
+                  src="./images/Group.svg"
+                  alt=""
+                  width={240}
+                  height={28}
+                />
+              </span>
             </div>
-            <div className="flex lg:items-center gap-1 -mt-24">
-              <div className="max-width-[374px] p-4">
-                <Image
-                  src="./images/Digital_Marketing.svg"
-                  alt="Digital marketing"
-                  width={374}
-                  height={374}
-                />
-              </div>
-              <div className="w-[200px] lg:w-[463px] lg:h-[93px] max-lg:-rotate-45">
-                <Image
-                  src="./images/Vector21.svg"
-                  alt="twisted arrow"
-                  width={463}
-                  height={93}
-                />
-              </div>
-              <div className="max-width-[374px]">
-                <Image
-                  src="./images/g10.svg"
-                  alt="blink signal"
-                  width={374}
-                  height={374}
-                />
-              </div>
+            <p className="text-natural-3 text-sm md:text-base font-bold py-8">
+              Elevate your online presence with Spine cutting-edge solutions.
+              Discover limitless possibilities for your digital agency&apos;s
+              success.
+            </p>
+            <Button label="Start for free" />
+          </div>
+          <div className="flex justify-center lg:items-center -mt-24 max-md:-rotate-[45deg] max-md:mx-4 max-md:mt-[100px] px-4">
+            <div className="min-w-[176px] max-w-[374px] max-md:rotate-[45deg] max-md:ml-8 max-md:-mr-8 max-md:mt-[-60px]">
+              <Image
+                src="./images/Digital_Marketing.svg"
+                alt="Digital marketing"
+                width={374}
+                height={374}
+              />
             </div>
-          </section>
-        </div>
+            <div className="min-w-[125px] min-h-[25px] lg:w-[463px] lg:h-[93px] max-md:rotate-[5deg] max-md:mt-24 mt-32 lg:mt-24">
+              <Image
+                src="./images/Vector21.svg"
+                alt="twisted arrow"
+                width={463}
+                height={93}
+              />
+            </div>
+            <div className="min-w-[176px] max-w-[374px] max-md:rotate-[45deg]">
+              <Image
+                src="./images/bwink_edu.svg"
+                alt="blink signal"
+                width={374}
+                height={374}
+              />
+            </div>
+          </div>
+        </section>
       </div>
     </header>
   );
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,24 +1,78 @@
+'use client';
+
 import { navLinks } from '@/data/data';
 import Image from 'next/image';
 import Link from 'next/link';
 import Button from '../ui/button';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
-    <div className="flex justify-between items-center font-bold py-8">
-      <Link href="#">
-        <Image src="./images/logo.svg" alt="Spine" width={143} height={28} />
-      </Link>
-      <nav>
-        <ul className="flex justify-between gap-16">
-          {navLinks.map(({ href, name }) => (
-            <li key={href}>
-              <Link href={href}>{name}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <Button type="outline" label="Sign Up" />
+    <div>
+      <div className="hidden md:flex justify-between items-center font-bold py-8">
+        <Link href="#" onClick={handleClose}>
+          <Image src="./images/logo.svg" alt="Spine" width={143} height={28} />
+        </Link>
+        <nav>
+          <ul className="flex justify-between gap-16">
+            {navLinks.map(({ href, name }) => (
+              <li key={href}>
+                <Link href={href}>{name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Button type="outline" label="Sign Up" />
+      </div>
+      <div>
+        <div className="md:hidden flex justify-between items-center font-bold py-8 pb-24">
+          <Link href="#" onClick={handleClose}>
+            <Image
+              src="./images/logo.svg"
+              alt="Spine"
+              width={143}
+              height={28}
+            />
+          </Link>
+          <button onClick={() => setOpen(!open)}>
+            {!open ? (
+              <Image
+                src="./icons/Frame.svg"
+                alt="menu"
+                width={36}
+                height={36}
+              />
+            ) : (
+              <Image
+                src="./icons/close.svg"
+                alt="close"
+                width={36}
+                height={36}
+              />
+            )}
+          </button>
+        </div>
+        {open && (
+          <div className="flex flex-col gap-16 justify-start items-center w-full h-screen overflow-y-none pb-24">
+            <ul className="flex flex-col justify-between gap-16">
+              {navLinks.map(({ href, name }) => (
+                <li key={href}>
+                  <Link onClick={handleClose} href={href}>
+                    {name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <Button type="outline" label="Sign Up" onClick={handleClose} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
