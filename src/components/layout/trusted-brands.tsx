683d8ca279744c328762371cd310792df2950dd2
Irina Dimcheva <dimchevairina@gmail.com>
2024-06-09 12:59:08 +0300

    Add slider to trusted brands

--- src/components/layout/trusted-brands.tsx
+++ src/components/layout/trusted-brands.tsx
@@ -1,18 +1,46 @@
+'use client';
+
+import React from 'react';
+import Slider from 'react-slick';
 import { logos } from '@/data/data';
 import Divider from '../ui/divider';
 import Image from 'next/image';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function TrustedBrands() {
+  const settings = {
+    dots: false,
+    infinite: false,
+    slidesToShow: 5,
+    // slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="max-w-screen-fit mx-auto py-6 md:py-[80px]">
       <Divider />
-      <ul className="flex gap-12 justify-between overflow-x-auto w-full">
-        {logos.map(({ name, src }) => (
-          <li key={name} className="min-w-[76px] overflow-x-auto">
-            <Image src={src} alt={name} height={40} width={160} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <Slider {...settings}>
+          {logos.map(({ name, src }) => (
+            <div key={name} className="w-[76px] lg:w-[160px] overflow-hidden">
+              <Image src={src} alt={name} height={40} width={160} />
+            </div>
+          ))}
+        </Slider>
+      </div>
       <Divider />
     </section>
   );
